4e38490da24161eb027a9c2973a1eebb830411ef
Kai Niessen <kai.niessen@ventum.de>
2018-05-29 22:55:24 +0200

    Correct Solution file

---
+++ EyetrackerProject/Data/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class oTests : ObservableCollection<Test>
+    {
+
+    }
+
+    public class Class1
+    {
+    }
+}
